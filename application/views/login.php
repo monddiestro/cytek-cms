edf9a6c88d8b1f1119248c59943ac61e4ff70535
zonel picadizo <zonel.picadizo@carmudi.com.ph>
2019-01-16 18:30:56 +0800

    login

--- application/views/login.php
+++ application/views/login.php
@@ -1,25 +1,28 @@
-<div class="container" style="margin-top:15%;">
-    <div class="col-sm-offset-4 col-sm-4">
+<div class="container" style="margin-top:10%;">
+    <div class="offset-4 col-sm-4">
         <?php if(!empty($this->session->flashdata('message'))): ?>
         <div class="alert alert-<?php echo $this->session->flashdata('class') ?>">
             <?php echo $this->session->flashdata('message'); ?>
         </div>
         <?php endif; ?>
         <?php echo form_open('admin/check_account') ?>
-        <div class="panel panel-default">
-            <div class="panel-heading">
+        <div class="card shadow">
+            <div class="card-header">
                 Login
             </div>
-            <div class="panel-body">
+            <div class="card-body">
                 <div class="form-group">
-                    <input type="text" class="form-control" name="email" placeholder="Email"/>
+                    <label for="email">Email</label>
+                    <input type="text" id="email" class="form-control" name="email" placeholder="Email"/>
                 </div>
                 <div class="form-group">
-                    <input type="password" class="form-control" name="password" placeholder="Password"/>
+                    <label for="password">Password</label>
+                    <a href="" style="float:right;"> <span class="pt-1 small text-muted">Forgot Password?</span> </a>
+                    <input type="password" id="password" class="form-control" name="password" placeholder="Password"/>
                 </div>
             </div>
-            <div class="panel-footer">
-                <button class="btn btn-primary" type="submit">Login</button>
+            <div class="card-footer text-center">
+                <button class="btn btn-primary shadow w-50" type="submit">Login</button>
             </div>
         </div>
         <?php echo form_close(); ?>
